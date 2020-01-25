0e824b6d9925a3969406f70b97451ec9313532c1
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-01-24 19:49:07 -0500

    Imported welcomepage into app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
-import Header from "./components/Header.js";
-
+import React from 'react';
+import Header from './components/Header.js';
+import WelcomePage from './components/WelcomePage';
 
 export default function App() {
-  return (
-    <main>
-      <Header />
-    </main>
-  );
+	return (
+		<main>
+			<Header />
+			<WelcomePage />
+		</main>
+	);
 }
