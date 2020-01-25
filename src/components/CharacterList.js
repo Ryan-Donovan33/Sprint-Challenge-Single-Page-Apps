70eb121edefa5a25bd8302075d48bdb7d3af470b
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-01-24 19:22:15 -0500

    Accessing data via axios

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+	// TODO: Add useState to track data from useEffect
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+	const [ characters, setCharacters ] = useState([]);
 
-  return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
+	useEffect(() => {
+		// TODO: Add API Request here - must run in `useEffect`
+		//  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+		Axios.get('https://rickandmortyapi.com/api/character/')
+			.then((res) => {
+				setCharacters(res.data);
+			})
+			.catch((error) => {
+				console.log('Server Error', error);
+			});
+	}, []);
+
+	return (
+		<section className="character-list">
+			<h2>TODO: `array.map()` over your state here!</h2>
+		</section>
+	);
 }
