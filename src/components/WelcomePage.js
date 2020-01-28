6057246a51d2eadbe4c27321551a8291311b3c63
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-01-27 20:21:59 -0500

    Styled the welcome page a little bit

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -3,6 +3,13 @@ import Card from './Card';
 import { Route, Link } from 'react-router-dom';
 import SearchForm from './SearchForm';
 import CharacterList from './CharacterList';
+import styled from 'styled-components';
+
+const Sect = styled.section`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
 
 export default function WelcomePage(props) {
 	// const router = (e) => {
@@ -10,16 +17,16 @@ export default function WelcomePage(props) {
 	// 	props.history.push('/character-list');
 	// };
 	return (
-		<section className="welcome-page">
+		<Sect>
 			<header>
 				<h1>Welcome to the ultimate fan site!</h1>
 				<img className="main-img" src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="rick" />
 
-				<Link to="/character-page">Character</Link>
+				{/* <Link to="/character-page">Character</Link> */}
 				<Route>
 					<CharacterList />
 				</Route>
 			</header>
-		</section>
+		</Sect>
 	);
 }
